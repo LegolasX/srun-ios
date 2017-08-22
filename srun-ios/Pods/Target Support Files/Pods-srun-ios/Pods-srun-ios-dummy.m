7e01fd6ae675e7de5777d8c32aeb7301410ce169
LegolasX <Legolas.Invoker@gmail.com>
2017-08-22 17:28:18 +0800

    init

---
+++ srun-ios/Pods/Target Support Files/Pods-srun-ios/Pods-srun-ios-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_srun_ios : NSObject
+@end
+@implementation PodsDummy_Pods_srun_ios
+@end
---
+++ srun-ios/srun-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  srun-ios
+//
+//  Created by Legolas Invoker on 2017/8/22.
+//  Copyright © 2017年 Legolas Invoker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
