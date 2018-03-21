dd64a460f3d1df92e9e7a2c3c64fbd445ba06931
LegolasX <Legolas.Invoker@gmail.com>
2018-03-22 00:44:49 +0800

    HTML praser almost done

--- srun-ios/SrunKit/HTMLparser.swift
+++ srun-ios/SrunKit/HTMLparser.swift
@@ -19,10 +19,28 @@ import Kanna
 ///   - html: html源
 /// - Returns: 解析出的值,若解析失败，则返回nil
 func getParsedValue(nodeName:String, attributeTitle:String, attributeValue:String, value:String?, html:String) -> String? {
+    return getParsedValueWithAddtion(nodeName: nodeName, attributeTitle: attributeTitle, attributeValue: attributeValue, value: value, html: html, addtion: nil)
+}
+
+/// 从HTML里解析出第一个匹配的节点
+///
+/// - Parameters:
+///   - nodeName: 节点名称 如div input
+///   - attributeTitle: 限定节点属性 如 class id
+///   - attributeValue: 属性的具体值 如 button
+///   - value: 需要的该节点的哪个其他属性，若为空则返回该节点的innerHTML
+///   - html: html源
+///   - addtion: 额外要加的内容
+/// - Returns: 解析出的值,若解析失败，则返回nil
+func getParsedValueWithAddtion(nodeName:String, attributeTitle:String, attributeValue:String, value:String?, html:String, addtion:String?) -> String? {
+    let path = "//\(nodeName)[@\(attributeTitle)='\(attributeValue)'\(addtion ?? "")]"
+    print(path)
+    return getParsedValueWith(path, from: html, for: value)
+}
+
+func getParsedValueWith(_ xpath:String, from html:String,for value:String?) -> String?{
     if let doc = try? HTML(html: html, encoding: .utf8) {
-        let path = "//\(nodeName)[@\(attributeTitle)='\(attributeValue)']"
-        print(path)
-        for link in doc.xpath(path) {
+        for link in doc.xpath(xpath) {
             if let value = value {
                 if let result = link["\(value)"] {
                     return result
@@ -34,5 +52,5 @@ func getParsedValue(nodeName:String, attributeTitle:String, attributeValue:Strin
             }
         }
     }
-    return nil
+    return nil;
 }
--- srun-ios/SrunKit/LRSrunManger.swift
+++ srun-ios/SrunKit/LRSrunManger.swift
@@ -63,30 +63,30 @@ public final class LRSrunManger: NSObject {
     
     public func logout(messageHandler:@escaping ((String) -> Void)) {
         let phoneURL = "http://202.204.67.15/srun_portal_phone.php"
-        let logoutParam = ["action" : "logout", "username" : "1141250201", "password" : "li777qing", "ajax" : "1"]
-        getRequest(url: phoneURL, with: logoutParam) { (response) in
+        guard let info = self.info, let action = self.action, let ip = self.userIP else { return }
+        let logoutParam = ["action" : action, "info" : info, "user_ip" : ip]
+        postRequest(url: phoneURL, with: logoutParam) { (response) in
             guard let data = response.data, let utf8Text = String(data: data, encoding: .utf8)  else { return }
             print(utf8Text)
-            messageHandler(utf8Text)
+            if let success = getParsedValue(nodeName: "div", attributeTitle: "id", attributeValue: "login_ok_date", value: nil, html: utf8Text) {
+                messageHandler(success.trimmingCharacters(in: .whitespacesAndNewlines))
+            }
         }
     }
-    
+    var userIP : String?
+    var action : String?
+    var info : String?
     public  func loginB(userName user:String, password:String, retryTime:NSInteger, messageHandler:@escaping ((String) -> Void)) {
         let loginPhoneParam = ["action" : "login","ac_id"    : "1","user_ip"    : "","nas_ip"    : "","user_mac"    : "","username"    : "1141250201","password"    : "li777qing","save_me"    : "1"]
         let phoneURL = "http://202.204.67.15/srun_portal_phone.php"
-        let html = "<!DOCTYPE html><html class=\"ui-mobile\">    <head id=\"ctl00_Head\">        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">        <base href=\".\">        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">        <meta name=\"format-detection\" content=\"telephone=no\">        <title>登录</title>        <link rel=\"stylesheet\" href=\"./css/bootstrap.min.css\">        <script src=\"./js/jquery-1.11.3.min.js\"></script>        <script src=\"./js/bootstrap.min.js\"></script>                    <script language=\"javascript\" src=\"js/srun_portal.js\"></script>    <style type=\"text/css\">        body{            background:url(images/phone1bg_1.png);            margin:0 auto;            padding:0px 0px;            margin:0px 0px;            height:500px;            }        .form-signin {            max-width:330px;            padding:15px;            margin:0 auto;            }        input {            margin-bottom:8px;            }        label {            margin-left:20px;            }        .navbar {            background-color:#1997ec;            text-align:center;            margin:0 auto;            border-bottom:1px solid #9C0;            }        .img-responsive {            text-align:center;            margin:0 auto;            }        .notice {            color:#C00;            }        .bg {            position:absolute;            z-index:-1;            height:289px;            width:280px;            bottom:0px;            left:0px;            background:url(images/phone1bg.png) no-repeat;            }        label {            font-weight:100;            padding:5px 5px;            }        .forget {            width:96%;            text-align:right;            }        .forget a {            text-decoration:underline;            }    </style>            </head>    <body class=\"ui-mobile-viewport ui-overlay-b\" onkeydown=\"BindEnter(event)\"><nav class=\"navbar\">    <img src=\"images/phone1logo.png\" width=\"330\" height=\"110\" border=\"0\" /></nav><nav class=\"bg\"></nav><div class=\"container\">                                <h5><br/></h5>                                                <form name=\"form3\" method=\"post\" action=\"srun_portal_phone.php?ac_id=1\" id=\"login_form\" class=\"form-signin\" onSubmit=\"return do_login()\">                            <input type=\"hidden\" name=\"action\" value=\"auto_logout\">                            <input type=\"hidden\" name=\"info\" value=\"bQ0pOyR6IX/u0akbf5QES0glrNeCGpyO0W6Rspy5eqNn6nc4gW3fNXOwIvKINohEwt+I5+9eRM4aYtjFVXhJvUolIug5QGD7xuiOkHn3ImcShwTFxKwf7RaYNUs8elDe96NZMMOkS6qkGJtYr2iRCou7/TCEREAbQ4OhUYtTODhO4iF6Jt51Iv1F6XDA\">                            <input type=\"hidden\" name=\"user_ip\" value=\"10.112.174.75\">                            <div style=\" text-align: center; padding:20px 0 20px;font-size: 24px; color:orange\" id=\"login_ok_date\">                                网络已连接                            </div>                                                                                            <input type=\"submit\" class=\"btn btn-md btn-primary btn-block\" value=\"注销\">&nbsp;&nbsp;                                                                                        </form>                                    </div>"
-
-        if let value = getParsedValue(nodeName: "input", attributeTitle: "name", attributeValue: "info", value: "value", html: html) {
-            print(value)
-        }
-        if let value = getParsedValue(nodeName: "div", attributeTitle: "id", attributeValue: "login_ok_date", value: nil, html: html) {
-            print(value)
-        }
         postRequest(url: phoneURL, with: loginPhoneParam, success: { response in
             guard let data = response.data, let utf8Text = String(data: data, encoding: .utf8)  else { return }
             print(utf8Text)
-            if let value = getParsedValue(nodeName: "input", attributeTitle: "name", attributeValue: "info", value: "value", html: utf8Text) {
-                print(value)
+            self.info = getParsedValue(nodeName: "input", attributeTitle: "name", attributeValue: "info", value: "value", html: utf8Text)
+            self.action = getParsedValue(nodeName: "input", attributeTitle: "name", attributeValue: "action", value: "value", html: utf8Text)
+            self.userIP = getParsedValue(nodeName: "input", attributeTitle: "name", attributeValue: "user_ip", value: "value", html: utf8Text)
+            if let success = getParsedValue(nodeName: "div", attributeTitle: "id", attributeValue: "login_ok_date", value: nil, html: utf8Text) {
+                messageHandler(success.trimmingCharacters(in: .whitespacesAndNewlines))
             }
         })
     }
@@ -94,7 +94,17 @@ public final class LRSrunManger: NSObject {
     public  func status(messageHandler:@escaping ((String) -> Void)) {
         getRequest(url: "http://202.204.67.15/srun_portal_pc_succeed.php", with: nil) { (response) in
             guard let data = response.data, let utf8Text = String(data: data, encoding: .utf8)  else { return }
-            print(utf8Text)
+//            print(utf8Text)
+            print(getParsedValueWith("//span[@id='sum_bytes'][2]", from: utf8Text, for: nil))
+            print(getParsedValueWith("//span[@id='sum_bytes'][1]", from: utf8Text, for: nil))
+            print(getParsedValueWith("//span[@id='sum_bytes'][0]", from: utf8Text, for: nil))
+
+//            guard let userBalance = getParsedValue(nodeName: "span", attributeTitle: "id", attributeValue: "user_balance", value: nil, html: utf8Text),
+//                let sumSeconds = getParsedValue(nodeName: "span", attributeTitle: "id", attributeValue: "sum_seconds", value: nil, html: utf8Text),
+////                let sumBytes = getParsedValueWithAddtion(nodeName: "span", attributeTitle: "id", attributeValue: "sum_bytes", value: nil, html: utf8Text, addtion:"1"),
+//                let userName = getParsedValue(nodeName: "span", attributeTitle: "id", attributeValue: "user_name", value: nil, html: utf8Text) else { return }
+//            print(userBalance,sumSeconds,sumBytes,userName)
+            
         }
     }
     
