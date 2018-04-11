3385b25e03fdcfc2112dc4bb70a1b24a09e06355
LegolasX <Legolas.Invoker@gmail.com>
2018-04-11 18:08:00 +0800

    integrate Fabric/Crashlytics

--- srun-ios/AppDelegate.swift
+++ srun-ios/AppDelegate.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 import CoreData
+import Fabric
+import Crashlytics
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +20,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //integrate Fabric/Crashlytics
+        Fabric.with([Crashlytics.self])
+
         return true
     }
 
